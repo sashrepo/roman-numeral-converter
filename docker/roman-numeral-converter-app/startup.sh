6976b718ee996692f87c8cc74fc7d455dea6860a
Saravanesh Selvaraj <sSelvaraj@ancestry.com>
2021-09-02 00:18:12 -0600

    remove trace level logging for elastic apm agent

--- docker/roman-numeral-converter-app/startup.sh
+++ docker/roman-numeral-converter-app/startup.sh
@@ -3,6 +3,4 @@ exec java \
   -Delastic.apm.service_name=roman-numeral-converter \
   -Delastic.apm.server_url=http://apm-server:8200 \
   -Delastic.apm.application_packages=com.converter \
-  -Delastic.apm.log_level=TRACE \
-  -Delastic.apm.log_file=_AGENT_HOME_/logs/elastic-apm.log \
   -jar app.jar
